b053eaec2de5a886e2cfe45c4e3292f7633adc8d
kinderst <kinders@uw.edu>
2024-01-06 13:46:57 -0700

    updated jobs

--- jobs/vit/from_pretrained/low_runs/epoch10_freeze0_bn_norm.sh
+++ jobs/vit/from_pretrained/low_runs/epoch10_freeze0_bn_norm.sh
@@ -11,7 +11,7 @@ conda activate pytorch_hf
 echo "== Starting! =="
 sleep 3
 python /projects/skinder@xsede.org/dmist_fl/code/dino/main_dino.py --arch vit_base --use_pretrained_dino True --epochs 10 --warmup_epochs 1 \
-    --freeze_last_layer 1 --use_bn_in_head True --norm_last_layer True \
-    --data_path /projects/skinder@xsede.org/dmist_fl/data/dmist_preprocessed_npys/ \
-    --output_dir /scratch/alpine/skinder@xsede.org/dmist_fl/dino_outputs/vit_base_frompretrained_epoch10_freeze1_bn_norm
+    --freeze_last_layer 0 --use_bn_in_head True --norm_last_layer True \
+    --data_path /scratch/alpine/skinder@xsede.org/dmist_fl/data/datasets/original/central/train_and_val/ \
+    --output_dir /scratch/alpine/skinder@xsede.org/dmist_fl/dino_outputs/vit_base_frompretrained_epoch10_freeze0_bn_norm
 echo "== End =="
--- jobs/vit/from_scratch/low_runs/epoch20_freeze0_bn.sh
+++ jobs/vit/from_scratch/low_runs/epoch20_freeze0_bn.sh
@@ -12,6 +12,6 @@ echo "== Starting! =="
 sleep 3
 python /projects/skinder@xsede.org/dmist_fl/code/dino/main_dino.py --arch vit_base --use_pretrained_dino False --epochs 20 --warmup_epochs 2 \
     --freeze_last_layer 0 --use_bn_in_head True --norm_last_layer False \
-    --data_path /projects/skinder@xsede.org/dmist_fl/data/dmist_preprocessed_npys/ \
+    --data_path /scratch/alpine/skinder@xsede.org/dmist_fl/data/datasets/original/central/train_and_val/ \
     --output_dir /scratch/alpine/skinder@xsede.org/dmist_fl/dino_outputs/vit_base_epoch20_freeze0_bn
 echo "== End =="
